b90e1b97bb377acff13140981e1ff237be205e3c
Kieran <kieranc.inbox@gmail.com>
2022-03-09 21:51:10 +1100

    Added in Raycasting

--- Assets/Generic RPG/Scripts/Mover.cs
+++ Assets/Generic RPG/Scripts/Mover.cs
@@ -7,11 +7,15 @@ namespace GenericRPG
     {
         [SerializeField] private Transform target;
         private NavMeshAgent playerNavMeshAgent;
+
+        private Camera mainCamera;
+        private Ray lastRay;
         // Start is called before the first frame update
         void Start()
         {
             playerNavMeshAgent = GetComponent<NavMeshAgent>();
 
+            mainCamera = Camera.main;
             if(playerNavMeshAgent == null)
                 playerNavMeshAgent = GetComponentInChildren<NavMeshAgent>();
         }
@@ -19,7 +23,17 @@ namespace GenericRPG
         // Update is called once per frame
         void Update()
         {
+            if(Input.GetMouseButtonDown(0))
+            {
+                MoveToCursor();
+            }
+            lastRay = mainCamera.ScreenPointToRay(Input.mousePosition);
             playerNavMeshAgent.SetDestination(target.position);
+        }
+
+        private void MoveToCursor()
+        {
+            
         }
     }
 }
